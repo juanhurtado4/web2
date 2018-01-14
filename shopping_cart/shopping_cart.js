ed40a3ed1af8a9ebd954adc563daef32f7387b48
juan hurtado <juanhurtado4@outlook.com>
2018-01-14 13:32:48 -0800

    Feat: Implement getAllItems function

--- shopping_cart/shopping_cart.js
+++ shopping_cart/shopping_cart.js
@@ -1,20 +1,27 @@
 let shoppingCart = {
     products: {},
 
-    addItem: (item) => {
+    addItem(item){
         this.products[item.id] = item;
     },
 
-    removeItem: (item) => {
+    removeItem(item){
         delete this.products[item.id]
+    },
+
+    get_all_items(){
+        return Object.values(this.products)
     }
+
+
+
 };
 
 class Product {
     constructor(name, price, id) {
         this.name = name;
-        this.price = price;
-        this.quantity = 0;
-        this.id = id + 1;
+        this.price = price * this.quantity;
+        this.quantity = 1;
+        this.id = id;
     }
 };
