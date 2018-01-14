0b24d4aea8283e01b56cf7bc859679237afe90c8
juan hurtado <juanhurtado4@outlook.com>
2018-01-14 13:35:40 -0800

    Feat: Implement increase and decrease quantity function

--- shopping_cart/shopping_cart.js
+++ shopping_cart/shopping_cart.js
@@ -9,10 +9,23 @@ let shoppingCart = {
         delete this.products[item.id]
     },
 
-    get_all_items(){
+    getAllItems(){
         return Object.values(this.products)
-    }
+    },
 
+    increaseQuantity(item){
+        this.products[item.id] += 1
+    },
+    
+    decreaseQuantity(item){
+        if (this.products[item.id] - 1 < 1) {
+            this.removeItem(item);
+        } else {
+            this.products[item.id] += 1;
+        }
+    },
+
+    
 
 
 };
