73e1c25d68ada064051ccbff410029ec90ac3fde
juan hurtado <juanhurtado4@outlook.com>
2018-01-14 22:07:06 -0800

    Chore: Finish learnyounode time_server challenge

--- learn_you_node/time_server.js
+++ learn_you_node/time_server.js
@@ -1,6 +1,18 @@
 let portNumber = process.argv[2];
 let net = require('net')
+
+function pad(n) { return n < 10 ? '0' + n : n }
+
+
 let server = net.createServer(function (socket) {
 
-})
-server.listen(portNumber)
+    let d = new Date();
+    let dateFormat = d.getFullYear() + '-' +
+        pad(d.getMonth() + 1) + '-' +
+        pad(d.getDate()) + ' ' +
+        pad(d.getHours()) + ':' +
+        pad(d.getMinutes()) + '\n';
+
+    socket.end(dateFormat);
+});
+server.listen(portNumber);
