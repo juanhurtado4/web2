787153213c759ee67c6f157bccda2dfe46c79880
juan hurtado <juanhurtado4@outlook.com>
2018-01-18 11:57:43 -0800

    Feat: Include new post route

--- reddit_clone/server.js
+++ reddit_clone/server.js
@@ -8,8 +8,13 @@ app.use(express.static('public')); // for css
 app.use(bodyPser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
-    res.render('home')
+    res.render('home', {pageTitle: 'Reddit Clone'})
 })
+
+app.get('/post/new', (req, res) => {
+    res.render('post_new')
+})
+
 
 app.listen('3000', () => {
     console.log('Server listening on port 3000')
