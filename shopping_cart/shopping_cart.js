da0aee8fdc3679c86b53942aa119655fd42939b1
juan hurtado <juanhurtado4@outlook.com>
2018-01-22 15:58:39 -0800

    Style: Add space for code readibility

--- shopping_cart/shopping_cart.js
+++ shopping_cart/shopping_cart.js
@@ -4,24 +4,24 @@ class ShoppingCart {
         this.products = {};
     }
 
-    addItem(item){
+    addItem(item) {
         this.products[item.id] = item;
     }
 
-    removeItem(item){
+    removeItem(item) {
         delete this.products[item.id]
     }
 
-    getAllItems(){
+    getAllItems() {
         return Object.values(this.products)
     }
 
-    increaseQuantity(item){
+    increaseQuantity(item) {
         item.quantity += 1
         item.updatePrice();
     }
-    
-    decreaseQuantity(item){
+
+    decreaseQuantity(item) {
         if (item.quantity - 1 < 1) {
             this.removeItem(item);
         } else {
@@ -39,11 +39,11 @@ class ShoppingCart {
         }
         return total
     }
-        
+
     getTotalQuantity() {
         return this.getTotal('quantity');
     }
-    
+
     getTotalPrice() {
         return this.getTotal('price');
     }
@@ -53,7 +53,7 @@ class Product {
     constructor(name, price, id) {
         this.name = name;
         this.quantity = 1;
-        this.price =  price;
+        this.price = price;
         this.id = id;
     }
 
@@ -62,21 +62,22 @@ class Product {
     }
 };
 
+module.exports = { ShoppingCart, Product }
 
 let test = true;
 if (test) {
     // TODO: Implement test for decrease quantity
-    
+
     let util = require('util');
-    
-    
+
+
     let shirt = new Product('shirt', 10, 1)
     let pants = new Product('pants', 20, 2)
     let hat = new Product('hat', 5, 3)
     let shoes = new Product('shoes', 100, 4)
-    
+
     let cart = new ShoppingCart();
-    
+
     cart.addItem(shirt);
     let allItemsTest = cart.getAllItems()
     let totalQuant = cart.getTotalQuantity()
@@ -85,28 +86,28 @@ if (test) {
     console.log('all items (shirt): ' + util.inspect(allItemsTest, false, null));
     console.log('total quant (1): ' + totalQuant)
     console.log('total price (10): ' + totalPrice)
-    
+
     cart.addItem(pants);
     let allItemsTest2 = cart.getAllItems()
     let totalQuant2 = cart.getTotalQuantity()
     let totalPrice2 = cart.getTotalPrice()
-    
+
     console.log('--------------------------------')
     console.log('second item')
     console.log('all items (shirt, pants): ' + util.inspect(allItemsTest2, false, null));
     console.log('total quant (2): ' + totalQuant2)
     console.log('total price (30): ' + totalPrice2)
-    
-    
+
+
     cart.increaseQuantity(shirt)
     console.log('--------------------------------')
     console.log('Increase quant')
-    
+
     console.log('all items (shirt, pants): ' + util.inspect(cart.getAllItems(), false, null));
     console.log('total quant (3): ' + cart.getTotalQuantity())
     console.log('total price (40): ' + cart.getTotalPrice())
     cart.removeItem(shirt);
     console.log('all items (pants): ' + util.inspect(cart.getAllItems(), false, null));
     console.log('total price (20): ' + cart.getTotalPrice())
-    
+
 }
