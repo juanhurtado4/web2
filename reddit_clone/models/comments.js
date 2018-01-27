f28ba913d8fd02bcdd04bf6e203689f9f0721aef
juan hurtado <juanhurtado4@outlook.com>
2018-01-26 20:02:04 -0800

    Feat: Write schema for comments

---
+++ reddit_clone/models/comments.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
+
+const CommentSchema = new Schema({
+    createdAt: { type: Date },
+    updatedAt: { type: Date },
+    content: { type: String, required: true }
+})
+
+CommentSchema.pre('save', (next) => {
+    // SET createdAt AND updatedAt
+    const now = new Date()
+    this.updatedAt = now
+    if (!this.createdAt) {
+        this.createdAt = now
+    }
+    next()
+})
+
+module.exports = mongoose.model('Comment', CommentSchema)
