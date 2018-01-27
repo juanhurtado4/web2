a9def5bef8b60108399ee339d3889d319026fb87
juan hurtado <juanhurtado4@outlook.com>
2018-01-26 21:09:50 -0800

    Feat: Add comments to post

--- reddit_clone/controllers/show.js
+++ reddit_clone/controllers/show.js
@@ -3,7 +3,7 @@ let Post = require('../models/post.js');
 module.exports = app => {
     app.get('/posts/:id', (req, res) => {
         let postId = req.params.id
-        Post.findById(postId).then(post => {
+        Post.findById(postId).populate('comments').then(post => {
             res.render('show', { post: post, pageTitle: `Reddit Clone/${post.title}` })
         }).catch((err) => {
             console.log(err);
