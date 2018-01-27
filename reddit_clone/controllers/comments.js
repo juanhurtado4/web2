ab63e555578afd9737c6dba456b121ec8ac47547
juan hurtado <juanhurtado4@outlook.com>
2018-01-26 20:50:30 -0800

    Feat: Connect comment with post with ref association

--- reddit_clone/controllers/comments.js
+++ reddit_clone/controllers/comments.js
@@ -1,11 +1,16 @@
 let Comment = require('../models/comments.js');
+let Post = require('../models/post');
 
 module.exports = app => {
     app.post('/posts/:postId/comments', (req, res) => {
         const comment = new Comment(req.body);
-
         comment.save().then(comment => {
-            res.redirect(`/posts`)
+            return Post.findById(req.params.postId)
+        }).then(post => {
+            post.comments.unshift(comment)
+            return post.save()
+        }).then(post => {
+            res.redirect('/posts')
         }).catch(err => {
             console.log(err);
             res.redirect(`/posts`)
