f299372942142ed8c4f5c0d518f05bc0393ccbe2
juan hurtado <juanhurtado4@outlook.com>
2018-01-28 00:11:14 -0800

    Test: Pass get total price in cart

--- shopping_cart/test_shopping_cart.js
+++ shopping_cart/test_shopping_cart.js
@@ -4,9 +4,11 @@ const { ShoppingCart, Product } = require('./shopping_cart')
 
 let item;
 let cart;
+let item2;
 beforeEach(() => {
     item = new Product('chicken', 10, 1);
     cart = new ShoppingCart();
+    item2 = new Product('cheesecake', 3, 27)
 })
 
 describe('Testing Product', () => {
@@ -49,16 +51,38 @@ it('Should remove items from cart', () => {
 // Stretch challenges
 it('Should update the count of items in the cart', () => {
     expect(cart.getTotalQuantity()).to.equal(0);
+
     cart.addItem(item);
     expect(cart.getTotalQuantity()).to.equal(1);
+
     expect(item.quantity).to.equal(1);
+
     cart.increaseQuantity(item)
     expect(cart.getTotalQuantity()).to.equal(2);
+
     expect(item.quantity).to.equal(2);
 });
 
 it('Should remove an item when its count is 0', () => {
+    expect(cart.getTotalQuantity()).to.equal(0);
 
+    cart.addItem(item);
+    expect(cart.getTotalQuantity()).to.equal(1);
+
+    cart.addItem(item2);
+    expect(cart.getTotalQuantity()).to.equal(2);
+
+    cart.decreaseQuantity(item);
+    expect(cart.getTotalQuantity()).to.equal(1);
+
+    cart.decreaseQuantity(item2);
+    expect(cart.getTotalQuantity()).to.equal(0);
 });
 
-it('Should return the total cost of all items in the cart');
+it('Should return the total cost of all items in the cart', () => {
+    expect(cart.getTotalPrice()).to.equal(0);
+    cart.addItem(item);
+    expect(cart.getTotalPrice()).to.equal(10);
+    cart.addItem(item2);
+    expect(cart.getTotalPrice()).to.equal(13);
+});
