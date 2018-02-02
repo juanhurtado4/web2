ab4921a358c7bad64bc02891fab903191804fd6c
juan hurtado <juanhurtado4@outlook.com>
2018-02-01 22:19:13 -0800

    Feat: Add currentuser to route for auth

--- reddit_clone/controllers/index.js
+++ reddit_clone/controllers/index.js
@@ -2,12 +2,14 @@ let Post = require('../models/post');
 
 module.exports = app => {
     app.get('/posts', (req, res) => {
+        const currentUser = req.user;
 
         Post.find({}).then((posts) => {
-            res.render('index', { pageTitle: 'Reddit Clone', posts: posts })
+            res.render('index', { pageTitle: 'Reddit Clone', posts: posts, currentUser: currentUser })
         }).catch((err) => {
             console.log(err.message);
         })
 
         // res.render('index', { pageTitle: 'Reddit Clone' })
-})}
+    })
+}
