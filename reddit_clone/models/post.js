4478d883ecbb0910c6152cc50af3842f05a81c31
juan hurtado <juanhurtado4@outlook.com>
2018-02-01 23:16:38 -0800

    Feat: Add author to schemas

--- reddit_clone/models/post.js
+++ reddit_clone/models/post.js
@@ -8,7 +8,8 @@ const PostSchema = new Schema({
     title: { type: String, required: true },
     url: { type: String, required: true },
     summary: { type: String, required: true },
-    comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }]
+    comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
+    author: { type: Schema.Types.ObjectId, ref: 'User', required: true }
 })
 
 PostSchema.pre('save', function(next) {
