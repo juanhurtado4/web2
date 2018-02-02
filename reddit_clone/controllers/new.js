92c4dc73a2e76565b40eff9701e69a2488313040
juan hurtado <juanhurtado4@outlook.com>
2018-02-01 23:09:15 -0800

    Feat: Show new post form only if user is loged in

--- reddit_clone/controllers/new.js
+++ reddit_clone/controllers/new.js
@@ -1,6 +1,10 @@
 module.exports = app => {
     app.get('/posts/new', (req, res) => {
         const currentUser = req.user;
-        res.render('new', { pageTitle: 'Reddit Clone/new post', currentUser: currentUser })
+        if (currentUser) {
+            res.render('new', { pageTitle: 'Reddit Clone/new post', currentUser: currentUser })
+        } else {
+            return res.status(401).send({ message: 'UNAUTHORIZED' }); // Unauthorized
+        }
     })
 }
