d1b6fb50c0c99e1e2c1eeec827dd15e806eacc70
juan hurtado <juanhurtado4@outlook.com>
2018-02-01 23:47:05 -0800

    Feat: Link comment with user

--- reddit_clone/controllers/comments.js
+++ reddit_clone/controllers/comments.js
@@ -1,10 +1,17 @@
 let Comment = require('../models/comments.js');
 let Post = require('../models/post');
+let User = require('../models/user');
 
 module.exports = app => {
     app.post('/posts/:postId/comments', (req, res) => {
         const comment = new Comment(req.body);
+        comment.author = req.user._id
+
         comment.save().then(comment => {
+            return User.findById(req.user._id)
+        }).then(user => {
+            user.comments.unshift(comment);
+            user.save()
             return Post.findById(req.params.postId)
         }).then(post => {
             post.comments.unshift(comment)
