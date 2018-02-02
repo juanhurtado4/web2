e85588df4e7cbaaac38d62684ecc72943a374859
juan hurtado <juanhurtado4@outlook.com>
2018-02-01 23:30:10 -0800

    Feat: Link post with user

--- reddit_clone/controllers/create.js
+++ reddit_clone/controllers/create.js
@@ -1,11 +1,17 @@
 let Post = require('../models/post.js')
+let User = require('../models/user.js')
+
 
 module.exports = app => {
     app.post('/posts', (req, res) => {
         let post = new Post(req.body);
+        post.author = req.user._id
 
         post.save().then((post) => {
-            console.log(post)
+            return User.findById(req.user._id)
+        }).then(user => {
+            user.posts.unshift(post);
+            user.save();
             res.redirect('/posts')
         }).catch((err) => {
             console.log(err.message)
