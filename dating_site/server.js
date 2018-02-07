5d99d9587e0feeeb48019f39b9c6c9670927f1dc
juan hurtado <juanhurtado4@outlook.com>
2018-02-07 15:59:04 -0800

    Init:
    Dependencies for server

---
+++ dating_site/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const bodyPser = require('body-parser');
+const mongoose = require('mongoose');
+
+
+app.use(bodyPser.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost/27017');
+mongoose.set('debug', true);
+mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection Error:'))
+
+app.set('view engine', 'ejs');
