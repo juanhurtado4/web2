0c69cece16a9ddb9c00958555a59fe551178391b
juan hurtado <juanhurtado4@outlook.com>
2018-02-07 22:25:22 -0800

    Feat: Render home template on index route

--- dating_site/server.js
+++ dating_site/server.js
@@ -11,3 +11,11 @@ mongoose.set('debug', true);
 mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection Error:'))
 
 app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render(index, { pageTitle: 'A new dating website' })
+})
+
+app.listen('3000', () => {
+    console.log('Server listening on port 3000');
+})
