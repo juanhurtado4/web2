1a9d54cc07a46375363f90e25330513dc337e329
juan hurtado <juanhurtado4@outlook.com>
2018-02-12 14:35:52 -0800

    Refactor: Add two routes for home page

--- porto/controllers/index.js
+++ porto/controllers/index.js
@@ -1,6 +1,8 @@
 let Post = require('../models/post');
 
 function home(req, res) {
+    const currentUser = req.user;
+
     Post.find({}).then(posts => {
         res.render('index', { pageTitle: 'Porto: Showcase your skills', posts: posts, currentUser: currentUser })
     }).catch(err => {
@@ -9,15 +11,6 @@ function home(req, res) {
 }
 
 module.exports = app => {
-    app.get('/posts', (req, res) => {
-        const currentUser = req.user;
-
-        Post.find({}).then((posts) => {
-            res.render('index', { pageTitle: 'Reddit Clone', posts: posts, currentUser: currentUser })
-        }).catch((err) => {
-            console.log(err.message);
-        })
-
-        // res.render('index', { pageTitle: 'Reddit Clone' })
-    })
+    app.get('/', home);
+    app.get('/profiles', home);
 }
