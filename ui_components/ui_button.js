aa600df58d51f15dd390b1b062443cb447cdbfa5
juan hurtado <juanhurtado4@outlook.com>
2018-03-31 22:08:10 -0700

    Feat: Add js for ui

---
+++ ui_components/ui_button.js
@@ -0,0 +1,33 @@
+let num = document.getElementById("add");
+const btn = document.getElementById("btn");
+const btnReset = document.getElementById("btn-reset");
+
+const addOne = () => {
+    num.innerHTML = parseInt(num.innerHTML) + 1;
+};
+
+const reset = () => {
+    num.innerHTML = 0;
+};
+
+
+btn.addEventListener("mousedown", (e) => {
+    addOne();
+    num.classList.add("plus-ani")
+
+});
+
+btn.addEventListener("mouseup", (e) => {
+    num.classList.remove("plus-ani");
+});
+
+btnReset.addEventListener("mousedown", (e) => {
+    reset();
+    num.classList.add("reset-ani");
+})
+
+btnReset.addEventListener("mouseup", (e) => {
+    num.classList.remove("reset-ani");
+})
+
+
